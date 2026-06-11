bf439b482a6057b0241ca8ae7e9e8ed358978c0f
gaspardlbrg <gaspardleborgne@icloud.com>
2026-06-11 15:50:34 +0200

    Created the flashcards and carroussel logic.

---
+++ js/components.js
@@ -0,0 +1,69 @@
+class Header extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="/style/main.css" />
+
+      <header>
+          <h1>Flashcard PWA</h1>
+          <p>
+              Personal project to learn more about JavaScript and PWAs ·
+              Currently W.I.P.
+          </p>
+      </header>
+
+      `;
+  }
+}
+
+customElements.define("page-header", Header);
+
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="/style/main.css" />
+
+      <footer>
+          <p>
+              Made with love by
+              <a href="https://gaspard-lb.web.app/" target="_blank"
+                  >Gaspard Le Borgne</a
+              >, AI free
+          </p>
+      </footer>
+      `;
+  }
+}
+
+customElements.define("page-footer", Footer);
+
+/* CARD ELEMENT */
+
+class Flashcard extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="/style/cards.css" />
+
+      <div class="card">
+          <p class="recto"><slot name="recto"></slot></p>
+          <p class="verso hide"><slot name="verso"></slot></p>
+          <span class="turn">Cliquer pour révéler</span>
+      </div>
+      `;
+
+    const turns = this.shadowRoot.querySelectorAll(".turn");
+    const recto = this.shadowRoot.querySelectorAll(".recto");
+    const verso = this.shadowRoot.querySelectorAll(".verso");
+
+    for (let i = 0; i < turns.length; i++) {
+      turns[i].addEventListener("click", function () {
+        recto[i].classList.toggle("hide");
+        verso[i].classList.toggle("hide");
+      });
+    }
+  }
+}
+
+customElements.define("flash-card", Flashcard);
--- js/main.js
+++ js/main.js
@@ -8,3 +8,43 @@ if ("serviceWorker" in navigator) {
       .catch((err) => console.error("SW registation failed : ", err));
   });
 }
+
+// FLASHCARD CARROUSSEL
+
+const bck = document.querySelector("#back");
+const nxt = document.querySelector("#next");
+const cnter = document.querySelector("#counter");
+const cards = document.querySelectorAll(".cardd");
+let i = 0;
+
+function updateCarroussel() {
+  cnter.textContent = `${i + 1}/${cards.length}`;
+
+  for (let j = 0; j < cards.length; j++) {
+    if (j === i) {
+      cards[j].classList.remove("hide");
+    } else {
+      cards[j].classList.add("hide");
+    }
+  }
+}
+
+updateCarroussel();
+
+nxt.addEventListener("click", function () {
+  if (i === cards.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+  updateCarroussel();
+});
+
+bck.addEventListener("click", function () {
+  if (i === 0) {
+    i = cards.length - 1;
+  } else {
+    i--;
+  }
+  updateCarroussel();
+});
